ee9316f212308bf0833a8e1740535fb072540ce6
Julio Cesar Leyva Rodriguez <juliocesarleyvarodriguez@hotmail.com>
2024-12-03 11:08:20 -0700

    Changes in routes

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -26,4 +26,20 @@ $routes->group('admin', function ($routes) {
                 );
 
 
+    $routes->post('usuariosAlmacen/getUsuariosAlmacen'
+                , 'UsuariosAlmacenController::getUsuariosAlmacen'
+                ,['namespace' => 'julio101290\boilerplatestorages\Controllers']
+                );
+
+    $routes->get('almacen/usuariosPorAlmacen/(:any)'
+                , 'StoragesController::usuariosPorAlmacen/$1'
+                 ,['namespace' => 'julio101290\boilerplatestorages\Controllers']
+                );
+    
+    $routes->post('almacen/activarDesactivar'
+    , 'StoragesController::activarDesactivar'
+    ,['namespace' => 'julio101290\boilerplatestorages\Controllers']
+    );
+
+
 });
